235b9a7195a84b95660c969c0e6e47533aa8ff4e
mkalansari7 <mk.alansari7@gmail.com>
2022-02-09 19:14:48 +0300

    done

--- api/products/routes.js
+++ api/products/routes.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 
 const {
   getProducts,
@@ -6,25 +6,27 @@ const {
   productDelete,
   fetchProduct,
   productUpdate,
-} = require('./controllers');
+} = require("./controllers");
+
+const upload = require("../../middleware/multer");
 
 const router = express.Router();
 
-router.param('productId', async (req, res, next, productId) => {
+router.param("productId", async (req, res, next, productId) => {
   const product = await fetchProduct(productId, next);
   if (product) {
     req.product = product;
     next();
   } else {
-    const err = new Error('Product Not Found');
+    const err = new Error("Product Not Found");
     err.status = 404;
     next(err);
   }
 });
 
-router.get('/', getProducts);
-router.post('/', productCreate);
-router.delete('/:productId', productDelete);
-router.put('/:productId', productUpdate);
+router.get("/", getProducts);
+router.post("/", upload.single("image"), productCreate);
+router.delete("/:productId", productDelete);
+router.put("/:productId", upload.single("image"), productUpdate);
 
 module.exports = router;
